8afccb520e136afffe990fffbfe8caacb08adde6
niko-linux <geekgaming222@gmail.com>
2024-06-20 20:57:19 +0530

    Add BFS traversal to find shortest path

--- knights-travails.js
+++ knights-travails.js
@@ -1,4 +1,5 @@
-const knightMoves = [
+// All available moves from a position
+const knightMoveset = [
   [2, 1],
   [1, 2],
   [-1, 2],
@@ -9,6 +10,7 @@ const knightMoves = [
   [2, -1],
 ];
 
+// Generate chessboard using map
 function generateChessBoard() {
   const chessBoard = new Map();
   for (let x = 0; x < 8; x++) {
@@ -19,11 +21,12 @@ function generateChessBoard() {
   return chessBoard;
 }
 
+// Generate all possible moves for all squares
 function getMoves(chessBoard) {
   for (let x = 0; x < 8; x++) {
     for (let y = 0; y < 8; y++) {
       let moves = chessBoard.get(`${x},${y}`);
-      knightMoves.forEach((move) => {
+      knightMoveset.forEach((move) => {
         let newX = x + move[0];
         let newY = y + move[1];
         if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
@@ -33,8 +36,39 @@ function getMoves(chessBoard) {
       chessBoard.set(`${x},${y}`, moves);
     }
   }
-  console.log("Moves from (0,0):", chessBoard.get("0,0"));
-  console.log("Moves from (4,4):", chessBoard.get("4,4"));
 }
+
+// Find shortest path using BFS
+function knightMoves(start, end, chessBoard) {
+  let queue = [{ position: start, path: [start] }];
+  let visited = new Set();
+  visited.add(start.toString());
+
+  while (queue.length > 0) {
+    let { position, path } = queue.shift();
+
+    if (position[0] === end[0] && position[1] === end[1]) {
+      return path;
+    }
+    let moves = chessBoard.get(position.toString());
+    moves.forEach((move) => {
+      let moveKey = move.toString();
+      if (!visited.has(moveKey)) {
+        visited.add(moveKey);
+        queue.push({ position: move, path: [...path, move] });
+      }
+    });
+  }
+
+  return "Path not found";
+}
+
+// Driver code
 const chessBoard = generateChessBoard();
 getMoves(chessBoard);
+
+const start = [0, 0];
+const end = [7, 6];
+
+const path = knightMoves(start, end, chessBoard);
+console.log("Shortest path from [0,0] to [7,6]:", path);
